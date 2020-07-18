e279ca410dccf39dcf8e190b77b5f787657c759e
AsherBearce <49050298+AsherBearce@users.noreply.github.com>
2020-07-18 00:02:56 -0600

    Initial commit

---
+++ CharacterController/Assets/Scripts/MouseLook.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    public GameObject cameraController;
+    [Range(50, 100)]
+    public float mouseSensitivity;
+    private float pitch = 0;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * Time.deltaTime * mouseSensitivity;
+        float mouseY = Input.GetAxis("Mouse Y") * Time.deltaTime * mouseSensitivity;
+        pitch -= mouseY;
+        pitch = Mathf.Clamp(pitch, -90f, 90f);
+
+        cameraController.transform.localRotation = Quaternion.Euler(pitch, 0, 0);
+        transform.Rotate(mouseX * Vector3.up);
+    }
+}
