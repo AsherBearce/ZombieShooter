955111490966d36700ac7a620cd1219cb6ac0708
AsherBearce <49050298+AsherBearce@users.noreply.github.com>
2020-07-20 03:09:09 -0600

    Add torso tilting

--- CharacterController/Assets/Scripts/Player/AnimationController.cs
+++ CharacterController/Assets/Scripts/Player/AnimationController.cs
@@ -5,19 +5,54 @@ using UnityEngine;
 [RequireComponent(typeof(MouseLook))]
 public class AnimationController : MonoBehaviour
 {
-    public GameObject Head;
-    public GameObject Torso;
+    [Header("Rig Objects")]
+    public GameObject head;
+    public GameObject torso;
+    public GameObject rigBase;
+    public GameObject hips;
+    public GameObject rightArm;
+
+    [Header("Limb Track Objects")]
+    public GameObject trackPoint;
+
+    [Header("Animation Parameters")]
+    public float torsoAngle = 0;
+    [Range(0, 1)]
+    public float headlookAmount = 0.75f;
     private MouseLook mouseLook;
+    private Quaternion rightArmRotation;
+    private Vector3 rightArmPosition;
 
     private void Start()
     {
         mouseLook = transform.GetComponent<MouseLook>();
+        rightArmRotation = rightArm.transform.localRotation;
+    }
+
+    public void trackArm(GameObject arm, Vector3 worldSpaceTarget, Quaternion startRotation)
+    {
+        //This needs adjustment
+        arm.transform.localRotation = startRotation;
+        Vector3 startFrom = -arm.transform.up;
+
+        Vector3 localSpace = arm.transform.InverseTransformPoint(worldSpaceTarget);
+        Vector3 axis = Vector3.Cross(localSpace, -startFrom);
+        float angle = Vector3.Angle(localSpace, startFrom);
+
+        arm.transform.Rotate(axis, angle);
     }
 
     // Update is called once per frame
     void Update()
     {
-        Head.transform.localRotation = Quaternion.Euler(mouseLook.pitch / 2, 0, 0);
-        Torso.transform.localRotation = Quaternion.Euler(mouseLook.pitch / 2 + 180f, -90f, 0);
+        if (head != null)
+        {
+            head.transform.localRotation = Quaternion.Euler(0, torsoAngle, 0) * Quaternion.Euler(mouseLook.pitch * headlookAmount, 0, 0);
+            torso.transform.localRotation = Quaternion.Euler(mouseLook.pitch * ( 1 - headlookAmount) + 180f, -90f, 0) * Quaternion.Euler(0, -torsoAngle, 0);
+            hips.transform.localRotation = Quaternion.Euler(0, torsoAngle, 0);
+
+
+            trackArm(rightArm, trackPoint.transform.position, rightArmRotation);
+        }
     }
 }
