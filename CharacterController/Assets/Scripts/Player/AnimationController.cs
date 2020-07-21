4fb392bb819a97054a3d4996528c98697f741e88
AsherBearce <49050298+AsherBearce@users.noreply.github.com>
2020-07-20 23:52:01 -0600

    Update animation controller

--- CharacterController/Assets/Scripts/Player/AnimationController.cs
+++ CharacterController/Assets/Scripts/Player/AnimationController.cs
@@ -11,6 +11,7 @@ public class AnimationController : MonoBehaviour
     public GameObject rigBase;
     public GameObject hips;
     public GameObject rightArm;
+    public GameObject leftArm;
 
     [Header("Limb Track Objects")]
     public GameObject trackPoint;
@@ -19,27 +20,58 @@ public class AnimationController : MonoBehaviour
     public float torsoAngle = 0;
     [Range(0, 1)]
     public float headlookAmount = 0.75f;
+    [Range(0, 1)]
+    public float armOffsetLimit;
+
+    private const float limbLength = 2;
     private MouseLook mouseLook;
+    //Right arm
     private Quaternion rightArmRotation;
     private Vector3 rightArmPosition;
+    //Left arm
+    private Quaternion leftArmRotation;
+    private Vector3 leftArmPosition;
 
     private void Start()
     {
         mouseLook = transform.GetComponent<MouseLook>();
         rightArmRotation = rightArm.transform.localRotation;
+        rightArmPosition = rightArm.transform.localPosition;
+        leftArmRotation = leftArm.transform.localRotation;
+        leftArmPosition = leftArm.transform.localPosition;
     }
 
-    public void trackArm(GameObject arm, Vector3 worldSpaceTarget, Quaternion startRotation)
+    public void trackArm(GameObject arm, Vector3 worldSpaceTarget, Quaternion startRotation, Vector3 startPosition)
     {
-        //This needs adjustment
+        arm.transform.localPosition = startPosition;
         arm.transform.localRotation = startRotation;
-        Vector3 startFrom = -arm.transform.up;
+
+        Vector3 startFrom = arm.transform.InverseTransformDirection(arm.transform.up);
 
         Vector3 localSpace = arm.transform.InverseTransformPoint(worldSpaceTarget);
         Vector3 axis = Vector3.Cross(localSpace, -startFrom);
         float angle = Vector3.Angle(localSpace, startFrom);
 
+        float distFromHand = (arm.transform.position - worldSpaceTarget).magnitude - limbLength / 4;
+        distFromHand = Mathf.Clamp(distFromHand, -armOffsetLimit / 2, armOffsetLimit / 2);
+        
+
         arm.transform.Rotate(axis, angle);
+        arm.transform.position -= distFromHand * (arm.transform.position - worldSpaceTarget).normalized;
+    }
+
+    public void trackLeg(GameObject leg, Vector3 worldSpaceTarget, Quaternion startRotation, Vector3 startPosition)
+    {
+        leg.transform.localPosition = startPosition;
+        leg.transform.localRotation = startRotation;
+
+        Vector3 startFrom = leg.transform.InverseTransformDirection(leg.transform.up);
+
+        Vector3 localSpace = leg.transform.InverseTransformPoint(worldSpaceTarget);
+        Vector3 axis = Vector3.Cross(localSpace, -startFrom);
+        float angle = Vector3.Angle(localSpace, startFrom);
+
+        leg.transform.Rotate(axis, angle);
     }
 
     // Update is called once per frame
@@ -52,7 +84,8 @@ public class AnimationController : MonoBehaviour
             hips.transform.localRotation = Quaternion.Euler(0, torsoAngle, 0);
 
 
-            trackArm(rightArm, trackPoint.transform.position, rightArmRotation);
+            trackArm(rightArm, trackPoint.transform.position, rightArmRotation, rightArmPosition);
+            //trackArm(leftArm, trackPoint.transform.position, leftArmRotation, leftArmPosition);
         }
     }
 }
